21309e496d1a79809af9158e39e96a040f261451
Barnz <hi@barnz.dev>
2022-01-30 15:44:34 +0000

    add solution

--- server.js
+++ server.js
@@ -16,6 +16,27 @@ app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/shorturl', (req, res) => {
+  const postUrl = req.body.url;
+  ​
+  if(postUrl && /^(ftp|http|https):\/\/[^ "]+$/.test(postUrl)) {
+    routes.push(postUrl);
+    ​
+    res.json({
+      original_url: postUrl,
+      short_url: routes.length
+    });
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+});
+​
+app.get('/api/shorturl/:index', (req, res) => {
+  const target = routes[req.params.index - 1];
+  ​
+  target ? res.redirect(target) : res.json({error: 'invalid id'});
+});
+​
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
