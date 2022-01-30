6748bda64329d1353dc752c44aac8b186484baee
Barnz <hi@barnz.dev>
2022-01-30 15:43:40 +0000

    cleanup

--- server.js
+++ server.js
@@ -2,21 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+let routes = [];
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
 });
 
 app.listen(port, function() {
